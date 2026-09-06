e9f4d07d818ad2681187097dd870f705f5ebee4c
Raind <127611616+Juemuren@users.noreply.github.com>
2026-09-06 22:43:25 +0800

    msys2: use whole-root persist

--- scripts/msys2/setup.ps1
+++ scripts/msys2/setup.ps1
@@ -1,4 +1,4 @@
-$config_path = "$(scoop prefix msys2)\etc\nsswitch.conf"
+$config_path = "$env:MSYS_ROOT\etc\nsswitch.conf"
 (Get-Content $config_path) -replace '^db_home:.*$', 'db_home: windows' | Set-Content $config_path -Encoding ascii
 
 msys2 -c 'pacman --needed --noconfirm -S zsh'
